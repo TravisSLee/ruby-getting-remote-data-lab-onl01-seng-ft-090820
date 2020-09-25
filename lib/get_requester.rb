591ae541107bbba56ef14ff73812485c29e70f8e
Travis Lee <travisslee223@gmail.com>
2020-09-25 17:51:10 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ require 'json'
 
 class GetRequester
   
-  def get_response_body(URL)
+  def get_response_body()
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
