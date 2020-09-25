4b00fc2ccf4a119145168ce734c0d856ab50174a
Travis Lee <travisslee223@gmail.com>
2020-09-25 17:51:44 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,6 +5,8 @@ require 'json'
 
 class GetRequester
   
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  
   def get_response_body()
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
